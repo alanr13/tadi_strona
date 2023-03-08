8d6eec733c4aa2bcf617e3542c142f427d4117aa
alanr13 <127034211+alanr13@users.noreply.github.com>
2023-03-08 01:34:57 +0100

    Update skrypt.js

--- index/skrypt.js
+++ index/skrypt.js
@@ -43,313 +43,367 @@ function showImage()
     e = Math.floor(Math.random()*pics.length);
     f = Math.floor(Math.random()*pics.length);  
 
-
-    document.getElementById('driver').src = `./drivers/${pics[a]}`
-    document.getElementById('driver2').src = `./drivers/${pics[e]}`
-    document.getElementById('driver3').src = `./drivers/${pics[f]}`
-
-
     if(a == 0)
     {   
         kier = "Max Verstappen";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 1)
     {
         kier = "Sergio Perez";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 2)
     {
         kier = "Fernando Alonso";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 3)
     {
         kier = "Carlos Sainz";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 4)
     {
         kier = "Lewis Hamilton";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 5)
     {
         kier = "Lance Stroll";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 6)
     {
         kier = "George Russell";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 7)
     {
         kier = "Valtteri Bottas";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 8)
     {
         kier = "Pierre Gasly";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 9)
     {
         kier = "Alexander Albon";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 10)
     {
         kier = "Yuki Tsunoda";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 11)
     {
         kier = "Logan Sargeant";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 12)
     {
         kier = "Kevin Magnussen";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 13)
     {
         kier = "Nyck de Vries";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 14)
     {
         kier = "Nico Hulkenberg";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 15)
     {
         kier = "Zhou Guanyu";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 16)
     {
         kier = "Lando Norris";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 17)
     {
         kier = "Charles Leclerc";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 18)
     {
         kier = "Esteban Ocon";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
     else if(a == 19)
     {
         kier = "Oscar Piastri";
         document.getElementById('nazwa').innerHTML = "2. " + kier;
+        document.getElementById('driver').src = `./drivers/${pics[a]}`
     }
 
     if(e == 0)
     {   
         kier = "Max Verstappen";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 1)
     {
         kier = "Sergio Perez";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 2)
     {
         kier = "Fernando Alonso";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 3)
     {
         kier = "Carlos Sainz";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 4)
     {
         kier = "Lewis Hamilton";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 5)
     {
         kier = "Lance Stroll";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 6)
     {
         kier = "George Russell";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 7)
     {
         kier = "Valtteri Bottas";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 8)
     {
         kier = "Pierre Gasly";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 9)
     {
         kier = "Alexander Albon";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 10)
     {
         kier = "Yuki Tsunoda";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 11)
     {
         kier = "Logan Sargeant";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 12)
     {
         kier = "Kevin Magnussen";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 13)
     {
         kier = "Nyck de Vries";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 14)
     {
         kier = "Nico Hulkenberg";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 15)
     {
         kier = "Zhou Guanyu";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 16)
     {
         kier = "Lando Norris";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 17)
     {
         kier = "Charles Leclerc";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 18)
     {
         kier = "Esteban Ocon";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
     else if(e == 19)
     {
         kier = "Oscar Piastri";
         document.getElementById('nazwa2').innerHTML = "1. " + kier;
+        document.getElementById('driver2').src = `./drivers/${pics[e]}`
     }
 
     if(f == 0)
     {   
         kier = "Max Verstappen";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 1)
     {
         kier = "Sergio Perez";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 2)
     {
         kier = "Fernando Alonso";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 3)
     {
         kier = "Carlos Sainz";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 4)
     {
         kier = "Lewis Hamilton";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 5)
     {
         kier = "Lance Stroll";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 6)
     {
         kier = "George Russell";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 7)
     {
         kier = "Valtteri Bottas";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 8)
     {
         kier = "Pierre Gasly";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 9)
     {
         kier = "Alexander Albon";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 10)
     {
         kier = "Yuki Tsunoda";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 11)
     {
         kier = "Logan Sargeant";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 12)
     {
         kier = "Kevin Magnussen";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 13)
     {
         kier = "Nyck de Vries";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 14)
     {
         kier = "Nico Hulkenberg";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 15)
     {
         kier = "Zhou Guanyu";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 16)
     {
         kier = "Lando Norris";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 17)
     {
         kier = "Charles Leclerc";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 18)
     {
         kier = "Esteban Ocon";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
     else if(f == 19)
     {
         kier = "Oscar Piastri";
         document.getElementById('nazwa3').innerHTML = "3. " + kier;
+        document.getElementById('driver3').src = `./drivers/${pics[f]}`
     }
 
 
